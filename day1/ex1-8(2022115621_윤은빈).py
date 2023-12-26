feacb2d586a1b9f73af2a0078d4f546218d687dc
yoon-0012 <yooneun48@gmail.com>
2023-12-26 18:07:31 +0900

    Complete the Week 1 lab
    1주 차 실습 완료

---
+++ day1/ex1-8(2022115621_윤은빈).py
@@ -0,0 +1,10 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import turtle
+>>> t = turtle.Pen()
+>>> t.shape("turtle")
+>>> t.forward(100)
+>>> t.up()
+>>> t.goto(0,100)
+>>> t.down()
+>>> t.forward(100)
---
+++ day1/ex1-9(2022115621_윤은빈).py
@@ -0,0 +1,11 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import turtle
+>>> t = turtle.Turtle()
+>>> t.shape("turtle")
+>>> t.circle(100)
+>>> t.up();t.goto(-200,0);t.down();t.circle(100)
+>>> t.up();t.goto(-100,-90);t.down();t.circle(100)
+>>> t.up();t.goto(0,0);t.down();t.circle(100)
+>>> t.up();t.goto(100,-90);t.down();t.circle(100)
+>>> t.up();t.goto(200,0);t.down();t.circle(100)
